b982c3dc7c49a17ec69bfe54aeae9673076ea27e
Michael Telatynski <7t3chguy@gmail.com>
2022-08-03 16:09:21 +0100

    Tweak help copy

--- gha-webhook-listener.py
+++ gha-webhook-listener.py
@@ -351,7 +351,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--workflow-pattern",
-        help=("Define a regex which artifact names must match."),
+        help=("Define a regex which workflow names must match. If omitted then will allow any."),
     )
 
     parser.add_argument(
